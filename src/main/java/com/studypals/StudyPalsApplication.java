6d9e24f027194fdaf4d336a0205eebafea38dd69
unikal1 <49333855+unikal1@users.noreply.github.com>
2025-03-27 14:04:27 +0900

    init commit

---
+++ src/main/java/com/studypals/StudyPalsApplication.java
@@ -0,0 +1,13 @@
+package com.studypals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyPalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studypals/StudyPalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studypals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
