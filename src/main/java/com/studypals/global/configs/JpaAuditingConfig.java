f71b7726e1b1195513f9ff2405695e758d756b00
unikal1 <jack8226123@gmail.com>
2025-04-06 18:33:58 +0900

    Fix : auditing 제거
    jpa auditing 을 위한 configuration 클래스 추가
    
    Ref #26

---
+++ src/main/java/com/studypals/global/configs/JpaAuditingConfig.java
@@ -0,0 +1,15 @@
+package com.studypals.global.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * jpa auditing 에 관한 configuration class
+ *
+ * @author jack8
+ * @since 2025-04-06
+ */
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
