aa99794ce169503329db806e16b6d3a69e78fd89
s0o0bn <0921soobin@gmail.com>
2025-04-13 00:51:45 +0900

    Test: 그룹 생성 시 예외 케이스의 단위 테스트 추가
    Ref: #46

--- src/test/java/com/studypals/domain/groupManage/service/GroupServiceTest.java
+++ src/test/java/com/studypals/domain/groupManage/service/GroupServiceTest.java
@@ -1,6 +1,7 @@
 package com.studypals.domain.groupManage.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -17,11 +18,13 @@ import com.studypals.domain.groupManage.dto.CreateGroupReq;
 import com.studypals.domain.groupManage.entity.Group;
 import com.studypals.domain.memberManage.dao.MemberRepository;
 import com.studypals.domain.memberManage.entity.Member;
+import com.studypals.global.exceptions.errorCode.GroupErrorCode;
+import com.studypals.global.exceptions.exception.GroupException;
 
 /**
- * {@link GroupService} 에 대한 테스트코드입니다.
+ * {@link GroupService} 에 대한 단위 테스트입니다.
  *
- * <p>성공 케이스에 대한 테스트입니다.
+ * <p>성공 케이스와 예외 케이스에 대한 테스트입니다.
  *
  * @author s0o0bn
  * @see GroupService
@@ -47,14 +50,7 @@ public class GroupServiceTest {
         // given
         Long userId = 1L;
         CreateGroupReq req = new CreateGroupReq("group name", "group tag", 10, false, false);
-        Group group = Group.builder()
-                .id(1L)
-                .name(req.name())
-                .tag(req.tag())
-                .maxMember(req.maxMember())
-                .isOpen(req.isOpen())
-                .isApprovalRequired(req.isApprovalRequired())
-                .build();
+        Group group = createGroup(req);
 
         given(memberRepository.getReferenceById(anyLong()))
                 .willReturn(Member.builder().id(userId).build());
@@ -65,5 +61,53 @@ public class GroupServiceTest {
 
         // then
         assertThat(actual).isEqualTo(group.getId());
+    }
+
+    @Test
+    void createGroup_fail_whileGroupSave() {
+        // given
+        Long userId = 1L;
+        GroupErrorCode errorCode = GroupErrorCode.GROUP_CREATE_FAIL;
+        CreateGroupReq req = new CreateGroupReq("group name", "group tag", 10, false, false);
+        Group group = createGroup(req);
+
+        given(groupRepository.save(any())).willThrow(new GroupException(errorCode));
+
+        // when & then
+        assertThatThrownBy(() -> groupService.createGroup(userId, req))
+                .isInstanceOf(GroupException.class)
+                .extracting("errorCode")
+                .isEqualTo(errorCode);
+    }
+
+    @Test
+    void createGroup_fail_whileGroupMemberSave() {
+        // given
+        Long userId = 1L;
+        GroupErrorCode errorCode = GroupErrorCode.GROUP_CREATE_FAIL;
+        CreateGroupReq req = new CreateGroupReq("group name", "group tag", 10, false, false);
+        Group group = createGroup(req);
+
+        given(memberRepository.getReferenceById(anyLong()))
+                .willReturn(Member.builder().id(userId).build());
+        given(groupRepository.save(any())).willReturn(group);
+        given(groupMemberRepository.save(any())).willThrow(new GroupException(errorCode));
+
+        // when & then
+        assertThatThrownBy(() -> groupService.createGroup(userId, req))
+                .isInstanceOf(GroupException.class)
+                .extracting("errorCode")
+                .isEqualTo(errorCode);
+    }
+
+    private Group createGroup(CreateGroupReq req) {
+        return Group.builder()
+                .id(1L)
+                .name(req.name())
+                .tag(req.tag())
+                .maxMember(req.maxMember())
+                .isOpen(req.isOpen())
+                .isApprovalRequired(req.isApprovalRequired())
+                .build();
     }
 }
