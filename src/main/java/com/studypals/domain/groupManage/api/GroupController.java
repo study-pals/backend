1199c0e2f9a6a514f83f526b89a727bf96c1547e
s0o0bn <0921soobin@gmail.com>
2025-04-12 18:50:37 +0900

    Feat: 그룹 생성 API 추가
    Ref: #46

---
+++ src/main/java/com/studypals/domain/groupManage/api/GroupController.java
@@ -0,0 +1,45 @@
+package com.studypals.domain.groupManage.api;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import com.studypals.domain.groupManage.dto.CreateGroupReq;
+import com.studypals.domain.groupManage.service.GroupService;
+import com.studypals.global.responses.CommonResponse;
+import com.studypals.global.responses.Response;
+import com.studypals.global.responses.ResponseCode;
+
+/**
+ * 그룹 관리에 대한 컨트롤러입니다. 담당하는 엔드포인트는 다음과 같습니다.
+ *
+ * <pre>
+ *     - POST /groups : 그룹 생성({@link CreateGroupReq})
+ * </pre>
+ *
+ * @author s0o0bn
+ * @since 2025-04-12
+ */
+@RestController
+@RequestMapping("/groups")
+@RequiredArgsConstructor
+public class GroupController {
+    private final GroupService groupService;
+
+    @PostMapping
+    public ResponseEntity<Response<Long>> createGroup(
+            @AuthenticationPrincipal Long userId, @Valid @RequestBody CreateGroupReq request) {
+        Long groupId = groupService.createGroup(userId, request);
+        Response<Long> response = CommonResponse.success(ResponseCode.GROUP_CREATE, groupId);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
