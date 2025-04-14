3610f877dd0df430acd279fb280e24522030f32a
unikal1 <jack8226123@gmail.com>
2025-04-14 20:10:26 +0900

    Refactor: 이름 변경 및 mapper 리펙토링
    Ref: #36

--- src/main/java/com/studypals/domain/studyManage/dto/mappers/StudyTimeMapper.java
+++ src/main/java/com/studypals/domain/studyManage/dto/mappers/StudyTimeMapper.java
@@ -1,9 +1,12 @@
 package com.studypals.domain.studyManage.dto.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import com.studypals.domain.studyManage.dto.GetStudyListDto;
 import com.studypals.domain.studyManage.dto.StartStudyDto;
 import com.studypals.domain.studyManage.entity.StudyStatus;
+import com.studypals.domain.studyManage.entity.StudyTime;
 
 /**
  * 코드에 대한 전체적인 역할을 적습니다.
@@ -16,4 +19,7 @@ import com.studypals.domain.studyManage.entity.StudyStatus;
 public interface StudyTimeMapper {
 
     StartStudyDto toDto(StudyStatus entity);
+
+    @Mapping(target = "categoryId", source = "category.id")
+    GetStudyListDto toDto(StudyTime entity);
 }
