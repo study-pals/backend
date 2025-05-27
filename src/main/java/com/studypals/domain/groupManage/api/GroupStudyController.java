28ff0328ec1820d377487ac0ddb1ccb791523ba2
s0o0bn <0921soobin@gmail.com>
2025-05-27 22:07:53 +0900

    Docs: 누락된 주석 추가
    GroupStudyController 클래스 주석

--- src/main/java/com/studypals/domain/groupManage/api/GroupStudyController.java
+++ src/main/java/com/studypals/domain/groupManage/api/GroupStudyController.java
@@ -14,6 +14,16 @@ import com.studypals.global.responses.CommonResponse;
 import com.studypals.global.responses.Response;
 import com.studypals.global.responses.ResponseCode;
 
+/**
+ * 그룹 루틴 관리에 대한 컨트롤러입니다. 담당하는 엔드포인트는 다음과 같습니다.
+ *
+ * <pre>
+ *     - GET /groups/{groupId}/routines/daily-goal : 오늘의 목표 조회
+ * </pre>
+ *
+ * @author s0o0bn
+ * @since 2025-05-11
+ */
 @RestController
 @RequestMapping("/groups/{groupId}/routines")
 @RequiredArgsConstructor
