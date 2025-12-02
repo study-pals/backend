47d0ffee36c18f8d3a1a83505f25bb273e360aa5
unikal1 <jack8226123@gmail.com>
2025-12-02 15:34:01 +0900

    Build: test container 설정 변경 및 일부 로깅 환경 추가
    Ref: #68

--- src/test/java/com/studypals/testModules/testSupport/TestEnvironment.java
+++ src/test/java/com/studypals/testModules/testSupport/TestEnvironment.java
@@ -1,10 +1,15 @@
 package com.studypals.testModules.testSupport;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+
+import java.util.Collections;
 
 /**
  *
@@ -19,17 +24,23 @@ public abstract class TestEnvironment {
     static final GenericContainer<?> REDIS;
     static final MongoDBContainer MONGO;
 
+    private static final Logger log = LoggerFactory.getLogger(TestEnvironment.class);
+
     static {
         MYSQL = new MySQLContainer<>("mysql:8.0")
                 .withDatabaseName("study_pal")
                 .withUsername("testuser")
-                .withPassword("testpassword");
+                .withPassword("testpassword")
+                .withCommand("--innodb_redo_log_capacity=512M", "--skip-log-bin")
+                .withTmpFs(Collections.singletonMap("/var/lib/mysql", "rw"))
+                .withLogConsumer(new Slf4jLogConsumer(log).withPrefix("MYSQL"))
+                .withReuse(true);
         MYSQL.start();
 
-        REDIS = new GenericContainer<>("redis:7.2").withExposedPorts(6379);
+        REDIS = new GenericContainer<>("redis:7.2").withExposedPorts(6379).withReuse(true);
         REDIS.start();
 
-        MONGO = new MongoDBContainer("mongo:7.0");
+        MONGO = new MongoDBContainer("mongo:7.0").withReuse(true);
         MONGO.start();
     }
 
