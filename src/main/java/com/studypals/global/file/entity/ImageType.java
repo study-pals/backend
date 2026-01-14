af3c70c3a3cc27905a84350e6a570c1eb8a128b1
sleepyhoon <pokjm99@gmail.com>
2026-01-14 17:32:18 +0900

    Feat: FileType 마커 인터페이스 도입

--- src/main/java/com/studypals/global/file/entity/ImageType.java
+++ src/main/java/com/studypals/global/file/entity/ImageType.java
@@ -1,10 +1,13 @@
 package com.studypals.global.file.entity;
 
 /**
- * 파일 이미지 타입을 정의합니다.
+ * 파일 이미지 타입을 정의합니다.<p>
  * 현재 프로필, 채팅 이미지만 고려합니다.
+ *
+ * @author sleepyhoon
+ * @since 2026-01-10
  */
-public enum ImageType {
+public enum ImageType implements FileType {
     PROFILE_IMAGE,
     CHAT_IMAGE
 }
